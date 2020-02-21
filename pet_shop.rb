d8aa0a5914c1913f6ab7f9f47de96be1597cfbff
Rob Mathews <robbis@gmail.com>
2020-02-21 14:03:36 +0000

    added add_or_remove_cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,9 +2,12 @@
    return pet_shop[:name]
  end
 
-def total_cash(pet_shop)
+def total_cash(pet_shop, cash = 0)
   return pet_shop[:admin][:total_cash]
 end
 
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+end
 
 # pet_shop[:pets].reduce(0) { |total, pet| total + pet[:price] }
