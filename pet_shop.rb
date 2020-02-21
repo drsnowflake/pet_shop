eb847f98e310a3254a4039ab76d340578718ec50
Rob Mathews <robbis@gmail.com>
2020-02-21 15:05:44 +0000

    finished extensions

--- pet_shop.rb
+++ pet_shop.rb
@@ -61,8 +61,13 @@ def customer_can_afford_pet(customer, new_pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  add_pet_to_customer(customer, pet)
-  increase_pets_sold(pet_shop, 1)
-  remove_customer_cash(customer, pet[:price])
-  add_or_remove_cash(pet_shop, pet[:price])
+  if pet == nil
+    return
+  end
+  if customer_can_afford_pet(customer, pet)
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop, pet[:price])
+  end
 end
