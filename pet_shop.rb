a2468de9d1560ecaee33a06494855f2611e6201d
Rob Mathews <robbis@gmail.com>
2020-02-21 13:57:17 +0000

    created/passed test_total_cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,10 @@
  def pet_shop_name(pet_shop)
    return pet_shop[:name]
  end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+
+# pet_shop[:pets].reduce(0) { |total, pet| total + pet[:price] }
